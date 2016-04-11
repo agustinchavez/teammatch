2679e38ddda5166ab7e5ac67d869b9b2d7b5af6c
agustinchavez <chavez13@illinois.edu>
2016-04-11 16:41:19 -0500

    tests for teams controller

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :team do
     name { Faker::Book.title }
     info { Faker::Hacker.say_something_smart }
-    admin_id {(FactoryGirl.create(:player)).id}
+    admin_id {FactoryGirl.create(:player).id}
   end
 end
