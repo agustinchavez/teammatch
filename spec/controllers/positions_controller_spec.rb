418b8aafcc96744c67f4c299258eeb352f0cc216
agustinchavez <chavez13@illinois.edu>
2016-04-11 16:37:43 -0500

    create destroy method for positions

--- spec/controllers/positions_controller_spec.rb
+++ spec/controllers/positions_controller_spec.rb
@@ -14,10 +14,6 @@ describe PositionsController do
      expect(@position).to be_a_kind_of(Position)
    end
 
-  it "redirects to root path when position is created" do
-      post :create, position: FactoryGirl.attributes_for(:position)
-      expect(response).to redirect_to root_path
-    end
   end
 
 end
