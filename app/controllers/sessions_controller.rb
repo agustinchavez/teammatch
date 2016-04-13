a6a9a47af2de991d40c16630a348d0f2d975a564
agustinchavez <chavez13@illinois.edu>
2016-04-13 11:05:31 -0500

    fix broken player and auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,28 @@ class SessionsController < ApplicationController
   def create
     auth = request.env['omniauth.auth']
     # session[:omniauth] =  auth.except('extra')
-    @authentication = Authentication.find_or_create_with_omniauth(auth)
-    @authentication.player = current_player
-    @authentication.save
-    redirect_to root_path, notice: "SIGNED IN"
-    # player = Authentication.create_with_omniauth(auth)
-    # session[:player_id] = player.id
-    # redirect_to root_path "Signed In"
+    @authentication = Authentication.find_with_omniauth(auth)
+
+    if @authentication.nil?
+     @authentication = Authentication.create_with_omniauth(auth)
+    end
+
+    if logged_in?
+      if @authentication.player == current_player
+        redirect_to root_path, notice: "SIGNED IN"
+      else
+        @authentication.player = session[:player_id]
+        @authentication.save
+        redirect_to root_path, notice: "SIGNED IN"
+      end
+     else
+       if @authentication.player.present?
+         self.current_player = @authentication.player
+        redirect_to root_path, notice: "SIGNED IN"
+       else
+        render :new
+      end
+    end
   end
 
   def destroy
