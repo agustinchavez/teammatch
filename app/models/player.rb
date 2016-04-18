e458687f91a9d83516447307f69d82f4a8123f87
agustinchavez <chavez13@illinois.edu>
2016-04-18 16:46:04 -0500

    added geokit and geolocations search

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,7 @@
 class Player < ActiveRecord::Base
   has_secure_password
+  acts_as_mappable :lat_column_name => :latitude,
+                   :lng_column_name => :longitude
 
   before_save :lat_long
 
