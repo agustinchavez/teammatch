efc4914734489f9a3f84fdc1627d3beb74798c89
agustinchavez <chavez13@illinois.edu>
2016-04-18 12:33:36 -0500

    fix Player controller add new players, and solve problems with profile page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    auth = request.env['omniauth.auth']
-    player = Player.find_or_create_by(provider: auth[:provider], uid: auth[:uid])
-    player.username = auth[:info][:name]
-    if player.new_record?
-      player.password = SecureRandom.uuid()
-      player.phone = '123345689'
-      player.save!
-    end
-    session[:player_id] = player.id
-    if player.positions == []
-       redirect_to welcome_edit_path
-     else
-       redirect_to root_path
-     end
+    # auth = request.env['omniauth.auth']
+    # player = Player.find_or_create_by(provider: auth[:provider], uid: auth[:uid])
+    # player.username = auth[:info][:name]
+    # if player.new_record?
+    #   player.password = SecureRandom.uuid()
+    #   player.phone = '123345689'
+    #   player.save!
+    # end
+    # session[:player_id] = player.id
+    # if player.positions == []
+    #    redirect_to welcome_edit_path
+    #  else
+    #    redirect_to root_path
+    #  end
   end
 
  def destroy
