518bd11b9c10b835279eb3a1f7a5883cfba4ca76
agustinchavez <chavez13@illinois.edu>
2016-04-19 11:19:18 -0500

    create youtube helper method to change youtube into usable widget and embedd media

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def youtube_embed_link  media
+    match = media.link.match(/v=(.{11})/)
+    if match
+      vid_code = match.captures[0]
+      "https://www.youtube.com/embed/#{vid_code}"
+    else
+      "https://www.youtube.com/embed/nonsense"
+    end
+  end
 end
