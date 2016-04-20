8832534f180a4bfce8cbca708d28120d3a99684b
agustinchavez <chavez13@illinois.edu>
2016-04-20 13:37:29 -0500

    add templates for emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
+  include ApplicationHelper
    default from: "chavez.agustin1991@gmail.com"
    layout 'mailer'
+
+    add_template_helper(ApplicationHelper)
+
+
+    def current_player
+      @current_player ||= Player.find_by(id: session[:player_id]) if session[:player_id]
+    end
  end
