cdff694f41ac1712fa48ba04d2ea581354280739
agustinchavez <chavez13@illinois.edu>
2016-04-20 10:12:04 -0500

    add code to be able to send emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,9 +82,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
+    :domain => "gmail.com",
     :user_name => Rails.application.secrets.gmail_username,
     :password => Rails.application.secrets.gmail_password,
-    :authentication => "plain",
+    :authentication => :plain,
   :enable_starttls_auto => true
 }
 end
