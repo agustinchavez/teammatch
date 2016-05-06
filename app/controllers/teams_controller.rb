9bd685a3179f2cc5c4f4a9152d8405874717cbbe
agustinchavez <chavez13@illinois.edu>
2016-05-06 10:53:44 -0500

    working on bugs in teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -54,81 +54,81 @@ class TeamsController < ApplicationController
     new_admin = params[:team][:admin_name]
     @team.admin = Player.find_or_create_by(username: new_admin.strip)
 
-    if params.has_key?("sport_types")
-      @sports = params[:sport_types]
-      @sports.each do |sport|
-        unless @team.sports.map(&:name).include?(sport)
-          @team.sports << Sport.find_or_create_by(name: sport.strip)
+      if params.has_key?("sport_types")
+        @sports = params[:sport_types]
+        @sports.each do |sport|
+          unless @team.sports.map(&:name).include?(sport)
+            @team.sports << Sport.find_or_create_by(name: sport.strip)
+          end
+        end
+      else
+        @team.sports.delete_all
+      end
+    end
+
+    new_members = params[:team][:members].split(",")
+
+    if new_members.any?
+      new_members.each do |member|
+        unless @team.players.map(&:username).include?(member)
+          @team.players << Player.find_or_create_by(username: member.strip)
         end
       end
-    else
-      @team.sports.delete_all
     end
-  end
 
-  new_members = params[:team][:members].split(",")
-
-  if new_members.any?
-    new_members.each do |member|
-      unless @team.players.map(&:username).include?(member)
-        @team.players << Player.find_or_create_by(username: member.strip)
-      end
-    end
-  end
-
-  @team.update_attributes(team_params)
-  redirect_to team_path(@team)
-end
-
-def edit
- @team = Team.find(params[:id])
- @sports = Sport.pluck(:name)
-end
-
-def destroy
-  @team = Team.find(params[:id])
-
-  if @team && params[:member]
-    PlayerTeam.find_by(player_id: params[:member]).destroy
+    @team.update_attributes(team_params)
     redirect_to team_path(@team)
-  elsif @team
-    @team.destroy
-    redirect_to root_path
-  else
-    @errors = @team.errors.full_messages
   end
-end
 
-def search
-  if params["Sport"]
-    sport_ids = params["Sport"].map {|e| e[0].to_i}
-    sports = Sport.find(sport_ids)
-    sports_with_teams = sports.select {|sport| !sport.teams.empty?}
-    teams_from_sport = sports_with_teams.map {|sport| sport.teams}.flatten
-    team_ids = teams_from_sport.map {|team| team.id}
-    @teams = Team.find(team_ids)
-    @teams_ids = @teams.map {|team| team.id}
-    render :"teams/_teams-location", layout: false
-  elsif params["Distance"]
-    teams_ids = params["teams"].split(" ").map {|e| e.to_i}
-    original_teams = Team.find(teams_ids)
-    player_location = [current_player.latitude, current_player.longitude]
-    distance = params["Distance"][0].to_i
-    all_teams_near = Team.within(distance, :origin => playr_location)
-    @teams = original_teams & all_teams_near
-    @map_string = "https://maps.googleapis.com/maps/api/staticmap?size=600x300&maptype=roadmap" + @teams.map {|team| team.map_string}.join("") + "&key=AIzaSyDbL6SGxiaR5BjXdzLPJHxQyjIAhoBVz_o"
-    render :"teams/_teams-sorted", layout: false
+  def edit
+   @team = Team.find(params[:id])
+   @sports = Sport.pluck(:name)
   end
-end
 
-def media
-  @team = Team.find(params[:id])
-  render partial:'team_media', locals: {team: @team}
-end
+  def destroy
+    @team = Team.find(params[:id])
 
-private
+    if @team && params[:member]
+      PlayerTeam.find_by(player_id: params[:member]).destroy
+      redirect_to team_path(@team)
+    elsif @team
+      @team.destroy
+      redirect_to root_path
+    else
+      @errors = @team.errors.full_messages
+    end
+  end
 
-def team_params
-  team_params = params.require(:team).permit(:team_name, :info)
-end
+  def search
+    if params["Sport"]
+      sport_ids = params["Sport"].map {|e| e[0].to_i}
+      sports = Sport.find(sport_ids)
+      sports_with_teams = sports.select {|sport| !sport.teams.empty?}
+      teams_from_sport = sports_with_teams.map {|sport| sport.teams}.flatten
+      team_ids = teams_from_sport.map {|team| team.id}
+      @teams = Team.find(team_ids)
+      @teams_ids = @teams.map {|team| team.id}
+      render :"teams/_teams-location", layout: false
+    elsif params["Distance"]
+      teams_ids = params["teams"].split(" ").map {|e| e.to_i}
+      original_teams = Team.find(teams_ids)
+      player_location = [current_player.latitude, current_player.longitude]
+      distance = params["Distance"][0].to_i
+      all_teams_near = Team.within(distance, :origin => playr_location)
+      @teams = original_teams & all_teams_near
+      @map_string = "https://maps.googleapis.com/maps/api/staticmap?size=600x300&maptype=roadmap" + @teams.map {|team| team.map_string}.join("") + "&key=AIzaSyDbL6SGxiaR5BjXdzLPJHxQyjIAhoBVz_o"
+      render :"teams/_teams-sorted", layout: false
+    end
+  end
+
+  def media
+    @team = Team.find(params[:id])
+    render partial:'team_media', locals: {team: @team}
+  end
+
+  private
+
+  def team_params
+    team_params = params.require(:team).permit(:team_name, :info, :admin, :members)
+  end
 end
